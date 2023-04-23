b4bdc7ed179b84abe58063414fa64826d67b8983
Pich-Chansopheak <chansopheak1201@gmail.com>
2023-04-23 21:53:46 +0700

    first

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+	$page = "home.php";
+	$slider = true;
+	$slidebar = true;
+	if(isset($_GET['p']))
+	{
+		$p= $_GET['p'];
+		switch($p)
+		{
+			case "shop":
+				$page = "shop.php";
+				$slider = false;
+				$slidebar = false;
+				break;
+			case "cart":
+				$page = "cart.php";
+				$slider = false;
+				$slidebar = false;
+				break;
+			case "blog":
+				$page = "blog.php";
+				$slider = false;
+				$slidebar = false;
+				break;
+			case "about":
+				$page = "about.php";
+				$slider = false;
+				$slidebar = false;
+				break;
+			case "contact":
+				$page = "contact.php";
+				$slider = false;
+				$slidebar = false;
+				break;
+			// case "signin":
+			// 		$page = "signin.php";
+			// 		$slider = false;
+			// 		$slidebar = false;
+			// 		break;
+		}
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include "includes/head.php"?>
+<body class="animsition">
+	
+<?php include "includes/header.php" ?>
+<?php include "includes/cart.php" ?>	
+
+<?php if($slider) include "includes/slider.php" ?>
+	<!-- Banner -->
+	<div class="sec-banner bg0 p-t-80 p-b-50">
+		<div class="container">
+        <?php if($slidebar) include "includes/slidebar.php" ?>
+        </div>
+	</div>
+
+
+<?php include $page ?>
+<?php include "includes/footer.php" ?>
+</body>
+</html>
---
+++ signin.php
@@ -0,0 +1,67 @@
+<?php include "includes/head.php"?>
+
+<div class="con-signin">
+<div class="form-signin">
+<div class="container-xxl">
+      <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+          <!-- Register -->
+          <div class="card">
+            <div class="card-body">
+              
+            <h3 class="sign-title">Sign In</h3>
+
+              <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email or Username</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="email"
+                    name="email-username"
+                    placeholder="Enter your email or username"
+                    autofocus
+                  />
+                </div>
+                <div class="mb-3 form-password-toggle">
+                  <div class="d-flex justify-content-between">
+                    <label class="form-label" for="password">Password</label>
+                    <!-- -->
+                  </div>
+                  <div class="input-group input-group-merge">
+                    <input
+                      type="password"
+                      id="password"
+                      class="form-control"
+                      name="password"
+                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      aria-describedby="password"
+                    />
+                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                  </div>
+                </div>
+                <div class="mb-3 ">
+                  <div class="form-check " style="margin-left:20px;">
+                    <input class="form-check-input"  type="checkbox" id="" />
+                    <label class="form-check-label p-0" for="remember-me"> Remember Me </label>
+                  </div>
+                </div>
+                <div class="mb-3">
+                  <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                </div>
+              </form>
+
+              <p class="text-center pb-3">
+                <span>New on our platform?</span>
+                <a href="signup.php">
+                  <span>Create an account</span>
+                </a>
+              </p>
+            </div>
+          </div>
+          <!-- /Register -->
+        </div>
+      </div>
+    </div>
+</div>
+</div>
